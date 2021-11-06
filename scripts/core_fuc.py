ad12c2d95eb87bb2c508fee77310a0ebf3886f34
Piop2 <seungyounyou0220@gmail.com>
2021-11-07 02:25:20 +0900

    update scripts.tile, scripts.core_fuc

--- scripts/core_fuc.py
+++ scripts/core_fuc.py
@@ -1,4 +1,5 @@
 import pygame
+import json
 
 def clip(surf,x,y,x_size,y_size):
     handle_surf = surf.copy()
@@ -6,3 +7,8 @@ def clip(surf,x,y,x_size,y_size):
     handle_surf.set_clip(clipR)
     image = surf.subsurface(handle_surf.get_clip())
     return image.copy()
+
+def read_f(path):
+    with open(path, 'r') as f:
+        data = json.load(f)
+    return data
--- scripts/tile.py
+++ scripts/tile.py
@@ -1,14 +1,29 @@
 import pygame
 import os
-from .core_fuc import clip
+from .core_fuc import *
 
 COLORKEY = (147, 187, 236)
-TILESETS_PATH = "data/images/tilesets"
 
 def load_tile(path):
     img = pygame.image.load(path)
     img.set_colorkey(COLORKEY)
     return img.copy()
 
-def load_all_tiles():
-    pass
+def load_tiles(path):
+    tiles = {}
+    tiles_animation_data = {}
+
+    tiles_path = os.listdir(path)
+    for tile_path in tiles_path:
+        if tile_path.split('.')[-1] == 'png':
+            tile_img = load_tile(f"{path}/{tile_path}")
+            tiles[int(tile_path.split('.')[0])] = tile_img
+
+            try:
+                data = read_f(tile_path.split('.')[0] + '.json')
+                tiles_animation_data[int(tile_path.split['.'][0])] = data
+
+            except FileNotFoundError:
+                pass
+    
+    return tiles, tiles_animation_data
